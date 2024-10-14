ca61d30d47c3936dc4d5dad6d4dbf6e0285d0730
Gabriel Duarte <gabrieldrtsr@gmail.com>
2024-10-14 02:11:23 -0300

    Comentários adicionados e valores de retorno da função main modificados

--- lab7/atividade3.c
+++ lab7/atividade3.c
@@ -14,6 +14,7 @@
 #define DEBUG_PRINT(x) do {} while (0)
 #endif
 
+/* Criado para diferenciar qual dos dois buffers será utilizado. */
 typedef enum _tipoBuffer
 {
     BUFFER1,
@@ -37,73 +38,88 @@ int main(int argc, char *argv[])
     pthread_t *tid;
     char *nomeArquivo;
 
+    /* Verificando se a quantidade de argumentos é a mínima necessária para a execução correta do programa. */
     if(argc < 3)
     {
         printf("Digite: %s <TAMANHO_BUFFER> <NOME_ARQUIVO_ENTRADA>\n", argv[0]);
         return 1;
     }
     tamanho = atoi(argv[1]);
+
+    /* Alocar o vetor de threads e verificar se pode ser alocado corretamente. */
     tid = malloc(sizeof(pthread_t) * NUMERO_THREADS);
     if(!tid)
     {
         puts("Não foi possível alocar espaço.");
         return 2;
     }
+
+    /* Verificando se o nome do arquivo não é nulo. */
     nomeArquivo = argv[2];
     if(nomeArquivo == NULL)
     {
         puts("Não foi possível ler o nome do arquivo.");
-        return 1;
+        return 3;
     }
+
+    /* Abertura do arquivo para leitura. */
     arquivoEntrada = fopen(nomeArquivo, "r");
     if(arquivoEntrada == NULL)
     {
         puts("Não foi possível abrir o arquivo para leitura.");
-        return 1;
+        return 4;
     }
+
+    /* Alocar e verificar se foram alocados ambos os buffers necessários. */
     buffer1 = malloc(sizeof(char) * tamanho);
     buffer2 = malloc(sizeof(char) * tamanho);
     if(buffer1 == NULL || buffer2 == NULL)
     {
         puts("Não foi possível abrir o arquivo para leitura.");
-        return 1;
+        return 5;
     }
 
-    /* Inicialização dos semáforos */
+    /* Inicialização dos semáforos. */
     sem_init(&slotsCheios1, 0, 0);
     sem_init(&slotsCheios2, 0, 0);
     sem_init(&slotsVazios1, 0, tamanho);
     sem_init(&slotsVazios2, 0, tamanho);
 
+    /* Criação de cada uma das 3 threads individualmente. */
     if(pthread_create(&tid[0], NULL, thread1, NULL))
     {
         puts("Erro no pthread_create.");
-        return 3;
+        return 6;
     }
     if(pthread_create(&tid[1], NULL, thread2, NULL))
     {
         puts("Erro no pthread_create.");
-        return 3;
+        return 7;
     }
     if(pthread_create(&tid[2], NULL, thread3, NULL))
     {
         puts("Erro no pthread_create.");
-        return 3;
+        return 8;
     }
 
+    /* Join nas threads e verificar se houve erro na função join. */
     for(int i = 0; i < NUMERO_THREADS; i++)
     {
         if(pthread_join(tid[i], NULL))
         {
             puts("Erro no pthread_join.");
-            return 4;
+            return 9;
         }
     }
 
+    /* Finalizar o programa liberando a memória do que foi utilizado. */
     sem_destroy(&slotsCheios1);
     sem_destroy(&slotsCheios2);
     sem_destroy(&slotsVazios1);
     sem_destroy(&slotsVazios2);
+    free(tid);
+    free(buffer1);
+    free(buffer2);
     return 0;
 }
 
@@ -113,7 +129,7 @@ void *thread1(void *arg)
     DEBUG_PRINT(("Thread 1 inicializada.\n"));
     while((caractere = fgetc(arquivoEntrada)) != EOF)
         insereBuffer(caractere, BUFFER1);
-    insereBuffer('\0', BUFFER1);
+    insereBuffer('\0', BUFFER1); // Adiciona '\0' no final do buffer 1 para sinalizar onde acaba o texto original.
     fclose(arquivoEntrada);
     DEBUG_PRINT(("\n\n\nThread 1 finaliada.\n\n\n"));
     return NULL;
@@ -124,7 +140,7 @@ void *thread2(void *arg)
     unsigned int caracteresLidos = 0;
     char retirado;
     DEBUG_PRINT(("Thread 2 inicializada.\n"));
-    while((retirado = retiraBuffer(BUFFER1)) != '\0')
+    while((retirado = retiraBuffer(BUFFER1)) != '\0') // Verifica se o texto original acabou caso o caractere lido seja '\0'
     {
         insereBuffer(retirado, BUFFER2);
         caracteresLidos++;
@@ -141,7 +157,7 @@ void *thread2(void *arg)
         {
             if(caracteresLidos >= LIMITE)
             {
-                insereBuffer('\n', BUFFER2);
+                insereBuffer('\n', BUFFER2); // Adiciona '\0' no final do buffer 2 para sinalizar onde acaba o texto original.
                 caracteresLidos = 0;
             }
         }
@@ -155,12 +171,13 @@ void *thread3(void *arg)
 {
     char retirado;
     DEBUG_PRINT(("Thread 3 inicializada.\n"));
-    while((retirado = retiraBuffer(BUFFER2)) != '\0')
+    while((retirado = retiraBuffer(BUFFER2)) != '\0') // Verifica se o texto original acabou caso o caractere lido seja '\0'
         printf("%c", retirado);
     DEBUG_PRINT(("Thread 3 finalizada.\n"));
     return NULL;
 }
 
+/* Função chamada pelo produtor. */
 void insereBuffer(char caractere, TipoBuffer tipo)
 {
     switch(tipo)
@@ -185,10 +202,13 @@ void insereBuffer(char caractere, TipoBuffer tipo)
             break;
         default:
             puts("Tipo não reconhecido passado como argumento da função 'insereBuffer'.");
-            exit(1);
+            exit(10);
     }
 }
 
+/* Função chamada pelo consumidor. 
+    @return Caractere consumido pelo consumidor.
+*/
 char retiraBuffer(TipoBuffer tipo)
 {
     char retorno;
@@ -214,7 +234,7 @@ char retiraBuffer(TipoBuffer tipo)
             break;
         default:
             puts("Tipo não reconhecido passado como argumento da função 'retiraBuffer'.");
-            exit(2);
+            exit(11);
     }
     return retorno;
 }
